5be3d29e5aacccee616e0b24ba5f3f16e36e11c2
Abdulmunim <amunim729@gmail.com>
2023-08-12 17:42:09 +0300

    Solved Missing Number

--- leetcode/MissingNumber.py
+++ leetcode/MissingNumber.py
@@ -1,3 +1,4 @@
+# MATHEMATICAL APPROACH
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # using mathematics: their sum all of them would be n(n+1)/2
@@ -5,3 +6,11 @@ class Solution:
         for num in nums:
             total -= num
         return total
+    
+## BruteForce O(N^2) Approach
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        numbers = list(range(len(nums)+1))
+        for num in nums:
+            numbers.remove(num)
+        return numbers[0]
