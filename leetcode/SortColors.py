3f71fa31772eebfe68ceb8fe86e52837cadc27f1
Abdulmunim <amunim729@gmail.com>
2023-08-16 21:23:25 +0300

    Improved Algorithm: 75. Sort Colors

--- leetcode/SortColors.py
+++ leetcode/SortColors.py
@@ -1,3 +1,21 @@
+# Current Solution: Improved Algorithm: Beats 97.5% Python3 Submissions - Aug 16 2023
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        red = nums.count(0)
+        white = nums.count(1)
+        blue = nums.count(2)
+        count = [red, white, blue]
+
+        index = 0
+        for color in range(3):
+            while count[color] > 0:
+                nums[index] = color
+                count[color] -= 1
+                index += 1
+
+
+# PREVIOUS SOLUTIONS
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         total = [0] * 3
