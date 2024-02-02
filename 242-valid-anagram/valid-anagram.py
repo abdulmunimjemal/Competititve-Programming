84d11f7c177921296c7b73a555f0242bd89dae26
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-02 18:22:25 +0300

    Time: 39 ms (44.91%) | Memory: 12.2 MB (93.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -7,10 +7,5 @@ class Solution(object):
         """
         if len(s) != len(t):
             return False
-        # using dictionary to keep number
-        alphabet = {letter: 0 for letter in string.ascii_lowercase}
-        for i in range(len(s)):
-            alphabet[s[i]] += 1
-            alphabet[t[i]] -= 1
-        return not any(alphabet.values())
+        return Counter(s) == Counter(t)
         
