b0ab8e4245489a71a169a95e71f70978d4ff94e5
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-02 22:34:30 +0300

    Time: 24 ms (99.63%) | Memory: 12.7 MB (83.83%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hash_table = {
-            num: i for i, num in enumerate(nums)
-        }
+        hash_table = dict()
         for i in range(len(nums)):
             value = target - nums[i]
             if value in hash_table and hash_table[value] != i:
                 return [hash_table[value], i]
+            else:
+                hash_table[nums[i]] = i
         return []
         
