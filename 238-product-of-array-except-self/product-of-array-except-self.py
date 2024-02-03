9d6bffadf797366cf231eeff0c74cf9017ab0bb1
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-03 10:17:29 +0300

    Time: 156 ms (95.00%) | Memory: 23.8 MB (89.89%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,26 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        max_product = 1
+        zero_indexes = []
+        for i in range(len(nums)):
+            num = nums[i]
+            if num != 0: 
+                max_product *= num
+            else:
+                zero_indexes.append(i)
+        result = []
+        for i in range(len(nums)):
+            number = nums[i]
+            if number != 0:
+                if zero_indexes:
+                    result.append(0)
+                else:
+                    result.append(max_product//number)
+            else:
+                if len(zero_indexes) > 1:
+                    result.append(0)
+                else:
+                    result.append(max_product)
+        return result
+
+        
