a2e7b91e9693670471b361807f8c72ac90203804
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-07 14:11:13 +0300

    Time: 90 ms (77.49%) | Memory: 16.6 MB (73.62%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # check all rows
+        for i in range(9):
+            row = set()
+            column  = set()
+
+            # traverse the whole row
+            for j in range(9):
+                val = board[i][j]
+                if val in row and val != '.': return False
+                row.add(val)
+            
+            # traverse the column corresponding to current row
+
+            for j in range(9):
+                val = board[j][i]
+                if val in column and val != '.': return False
+                column.add(val)
+    
+        # check 3 by 3 grid
+        # vertically dow first
+        for j in range(0,7,3):
+            for i in range(0,7,3):
+                unique = set()
+                for k in range(i,i+3):
+                    for l in range(j, j+3):
+                        val = board[k][l]
+                        if val in unique and val != '.': return False
+                        unique.add(val)
+                
+        return True
+                    
+
+
+
+
+
+
+
+
+        # check all columns
+
+        # check 3 by 3 box
+        
