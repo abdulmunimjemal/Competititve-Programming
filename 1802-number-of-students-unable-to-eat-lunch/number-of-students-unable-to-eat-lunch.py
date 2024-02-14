bbac3868d8220124f2adf8e56152cac791917ed7
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-14 22:16:23 +0300

    Time: 45 ms (35.06%) | Memory: 16.4 MB (95.16%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,16 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         skipped = 0
+        sandwiches = deque(sandwiches)
+        students = deque(students)
         while len(students) != skipped:
             if not (students and sandwiches): break
             if students[0] == sandwiches[0]:
-                students.pop(0)
-                sandwiches.pop(0)
+                students.popleft()
+                sandwiches.popleft()
                 skipped = 0
             else:
-                students.append(students.pop(0))
+                students.append(students.popleft())
                 skipped += 1
         return len(students)
         
