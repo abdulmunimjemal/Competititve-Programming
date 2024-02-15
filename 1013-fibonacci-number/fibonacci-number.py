ea18ec36411c848698b611b80d2831136e4ee288
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-15 23:26:54 +0300

    Time: 41 ms (50.31%) | Memory: 16.6 MB (62.78%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        context = {0:0, 1:1}
-        def find(n, context=context):
-            if n in context:
-                return context[n]
-            return find(n-1, context) + find(n-2, context)
-        return find(n)
+        a = 0
+        b = 1
+        if n <= 1: return n
+        for _ in range(2,n+1):
+            a, b = b, a + b
+        return b
