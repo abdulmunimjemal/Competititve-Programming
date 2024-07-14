9a450bead5f5cb4cb0c25d0fbd6e6cb174996d91
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-14 17:57:17 +0300

    Time: 42 ms (18.22%) | Memory: 11.8 MB (64.85%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,11 +1,16 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        # two pointers
-        array_1 = nums[:n]
-        array_2 = nums[n:]
-
-        result = []
-        for i in range(len(array_1)):
-            result.append(array_1[i])
-            result.append(array_2[i])
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        l1, l2 = nums[:n], nums[n:]
+        result = [0] * (2 * n)
+        for i in range(n):
+            result[2*i] = l1[i]
+            result[2*i + 1] = l2[i]
         return result
+
+
+        
