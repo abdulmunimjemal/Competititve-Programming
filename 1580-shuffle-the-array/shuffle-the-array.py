31f91d79a83d09d531745365ab591749b1b1cd76
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-14 17:57:49 +0300

    Time: 60 ms (23.44%) | Memory: 16.8 MB (41.28%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,16 +1,8 @@
-class Solution(object):
-    def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
         l1, l2 = nums[:n], nums[n:]
         result = [0] * (2 * n)
         for i in range(n):
             result[2*i] = l1[i]
             result[2*i + 1] = l2[i]
         return result
-
-
-        
