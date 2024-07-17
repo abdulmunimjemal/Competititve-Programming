8f9e7122f3979f3dfe93604b510398d92797bdfe
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-17 21:49:11 +0300

    Time: 56 ms (76.97%) | Memory: 18 MB (7.22%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hash_table = dict()
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for i in range(len(nums)):
-            value = target - nums[i]
-            if value in hash_table and hash_table[value] != i:
-                return [hash_table[value], i]
-            else:
-                hash_table[nums[i]] = i
-        return []
+            if nums[i] in hashmap:
+                return [i, hashmap[nums[i]]]
+            other = target - nums[i]
+            hashmap[other] = i
         
