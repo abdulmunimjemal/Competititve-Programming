14532b38d9a487fc5c5f6de38f90bc57366997a6
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-25 17:32:27 +0300

    Time: 169 ms (64.15%) | Memory: 18 MB (14.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = defaultdict(lambda : 0)
-        n = len(nums) / 2
-        for num in nums:
-            hashmap[num] += 1
-            if hashmap[num] >= n: return num
+        return Counter(nums).most_common(1)[0][0]
+
