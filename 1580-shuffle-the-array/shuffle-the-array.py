546e40cc2d716a307a914ce53138a777a81df987
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-08-02 16:22:59 +0300

    Time: 47 ms (93.34%) | Memory: 16.7 MB (83.14%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,3 +1,9 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        return [nums[i//2 + n*(i%2)] for i in range(2*n)]
+        first_half, last_half = nums[:n], nums[n:]
+        result = [0] * 2 * n
+        for i in range(n): # 2n and 2n+1
+            result[2*i] = first_half[i]
+            result[2*i + 1] = last_half[i]
+        return result
+        
